49bdec61365f98798cc4b1396b288a917f189c77
Ken Robertson <ken@invalidlogic.com>
2011-07-14 08:35:47 -0700

    Fixed issue with the Git provider failing to clone repositories to a destination that includes a space

--- chef/lib/chef/provider/git.rb
+++ chef/lib/chef/provider/git.rb
@@ -120,7 +120,7 @@ class Chef
 
         Chef::Log.info "#{@new_resource} cloning repo #{@new_resource.repository} to #{@new_resource.destination}"
 
-        clone_cmd = "git clone #{args.join(' ')} #{@new_resource.repository} #{@new_resource.destination}"
+        clone_cmd = "git clone #{args.join(' ')} #{@new_resource.repository} \"#{@new_resource.destination}\""
         shell_out!(clone_cmd, run_options(:command_log_level => :info))
       end
 
